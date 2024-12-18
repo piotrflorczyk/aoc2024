3d43d13cb5f7c6a0f0a2cdc590b5142f1483221c
unknown <florczykpiotr@yahoo.com>
2024-12-18 10:48:23 +0100

    day17

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -27,17 +27,22 @@ impl From<&str> for Opcode {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct State {
+    reg_a: u64,
+    reg_b: u64,
+    reg_c: u64,
+}
+
 #[derive(Debug, Clone)]
 struct Program {
-    reg_a: u32,
-    reg_b: u32,
-    reg_c: u32,
+    state: State,
     instr: Vec<Opcode>,
 }
 
-impl Program {
-    fn combo_to_value(&self, operand: Opcode) -> u32 {
-        let op = operand as u32;
+impl State {
+    fn combo_to_value(&self, operand: Opcode) -> u64 {
+        let op = operand as u64;
         match op {
             0..=3 => op,
             4 => self.reg_a,
@@ -48,13 +53,22 @@ impl Program {
     }
 }
 
+impl From<&str> for State {
+    fn from(value: &str) -> Self {
+        let lines = value.lines().collect::<Vec<_>>();
+        State {
+            reg_a: lines[0].split_once(": ").unwrap().1.parse::<u64>().unwrap(),
+            reg_b: lines[1].split_once(": ").unwrap().1.parse::<u64>().unwrap(),
+            reg_c: lines[2].split_once(": ").unwrap().1.parse::<u64>().unwrap(),
+        }
+    }
+}
+
 impl From<&str> for Program {
     fn from(value: &str) -> Self {
         let lines = value.lines().collect::<Vec<_>>();
         Program {
-            reg_a: lines[0].split_once(": ").unwrap().1.parse::<u32>().unwrap(),
-            reg_b: lines[1].split_once(": ").unwrap().1.parse::<u32>().unwrap(),
-            reg_c: lines[2].split_once(": ").unwrap().1.parse::<u32>().unwrap(),
+            state: State::from(value),
             instr: lines[4]
                 .split_once(": ")
                 .unwrap()
@@ -66,40 +80,105 @@ impl From<&str> for Program {
     }
 }
 
-fn p1() {
-    let mut program = Program::from(include_str!("../../input/day17"));
-
-    println!("{program:?}");
-
+fn emulate(program: &Program) -> Vec<u64> {
+    let mut state = program.state.clone();
     let mut pc = 0;
+    let mut output = Vec::new();
     while pc < program.instr.len() {
         match program.instr[pc] {
-            Opcode::Adv => program.reg_a /= 2u32.pow(program.combo_to_value(program.instr[pc + 1])),
-            Opcode::Bxl => program.reg_b ^= program.instr[pc + 1] as u32,
-            Opcode::Bst => program.reg_b = program.combo_to_value(program.instr[pc + 1]) % 8,
+            Opcode::Adv => {
+                state.reg_a /= 2u64.pow(state.combo_to_value(program.instr[pc + 1]) as u32)
+            }
+            Opcode::Bxl => state.reg_b ^= program.instr[pc + 1] as u64,
+            Opcode::Bst => state.reg_b = state.combo_to_value(program.instr[pc + 1]) % 8,
             Opcode::Jnz => {
-                if program.reg_a != 0 {
+                if state.reg_a != 0 {
                     pc = program.instr[pc + 1] as usize;
                     continue;
                 }
             }
-            Opcode::Bxc => program.reg_b ^= program.reg_c,
+            Opcode::Bxc => state.reg_b ^= state.reg_c,
             Opcode::Out => {
-                print!("{},", program.combo_to_value(program.instr[pc + 1]) % 8);
+                output.push(state.combo_to_value(program.instr[pc + 1]) % 8);
+                /* if output.len() == output_count {
+                    break;
+                }*/
             }
             Opcode::Bdv => {
-                program.reg_b =
-                    program.reg_a / (2u32.pow(program.combo_to_value(program.instr[pc + 1])))
+                state.reg_b =
+                    state.reg_a / 2u64.pow(state.combo_to_value(program.instr[pc + 1]) as u32)
             }
             Opcode::Cdv => {
-                program.reg_c =
-                    program.reg_a / (2u32.pow(program.combo_to_value(program.instr[pc + 1])))
+                state.reg_c =
+                    state.reg_a / 2u64.pow(state.combo_to_value(program.instr[pc + 1]) as u32)
             }
         }
         pc += 2
     }
+    output
+}
+
+fn p1() {
+    let program = Program::from(include_str!("../../input/day17"));
+    let out = emulate(&program);
+    let p1 = out
+        .iter()
+        .map(|el| format!("{}", el))
+        .collect::<Vec<_>>()
+        .join(",");
+    println!("P1: {p1}");
+}
+
+fn p2() {
+    let mut program = Program::from(include_str!("../../input/day17"));
+    let expected_output = program.instr.iter().map(|&x| x as u64).collect::<Vec<_>>();
+    let mut prev_valid: Vec<u64> = Vec::new();
+    let mut valid: Vec<u64> = Vec::new();
+    /*
+       Loop while A != 0 {
+           C = A >> ((A & 0b111) ^ 0b11) //[1]
+           B = (A & 0b111) ^ C //[2]
+           B = B ^ 0b11
+           OUT B & 0b111
+           A >>= 3
+       }
+       At each iteration we take last 3 bits [2] and and 3 bits from A >> [7-0]
+    */
+    for i in 1..=expected_output.len() {
+        if i == 1 {
+            // First iteration we check all 10 bits that could affect last element
+            for val in 0b0..=0b1_111_111_111 {
+                program.state.reg_a = val;
+                // run emulation and compare if the 1st elemente from output matches
+                let emu_out = emulate(&program);
+                if emu_out[0] == expected_output[0] {
+                    valid.push(val);
+                }
+            }
+        } else {
+            // to each element that was valid in previous step append every value from 0-0b111 and test
+            for val in 0b0..=0b111 {
+                prev_valid.iter().for_each(|v| {
+                    let reg_a_val = val << (7 + 3 * (i - 1)) | v;
+                    program.state.reg_a = reg_a_val;
+                    let emu_out = emulate(&program);
+                    if emu_out.len() <= expected_output.len()
+                        && emu_out.len() >= i
+                        && emu_out[..i] == expected_output[..i]
+                    {
+                        valid.push(reg_a_val);
+                    }
+                });
+            }
+        }
+        std::mem::swap(&mut prev_valid, &mut valid);
+        valid.clear();
+    }
+    let p2 = prev_valid[0];
+    println!("P2: {p2}");
 }
 
 fn main() {
     p1();
+    p2();
 }
