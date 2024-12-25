243fdcd9b53fade525477cfb3c68cb9f9c0f987b
Piotr Florczyk <florczyk.piotr@yahoo.com>
2024-12-25 12:30:18 +0100

    day24

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -9,11 +9,23 @@ struct Gate {
     op: char,
 }
 
-fn p1() {
+impl Gate {
+    fn gate_name(&self) -> String {
+        let op_name = match self.op {
+            '|' => "OR",
+            '&' => "AND",
+            '^' => "XOR",
+            _ => panic!("WTF"),
+        };
+        self.in1.clone() + "_" + op_name + "_" + &self.in2
+    }
+}
+
+fn load_input() -> (HashMap<String, u8>, HashMap<String, Gate>) {
     let (input_string, gates_string) = include_str!("../../input/day24")
         .split_once(DELIMITER)
         .unwrap();
-    let mut results = input_string
+    let results = input_string
         .lines()
         .map(|line| {
             let (name, val) = line.split_once(": ").unwrap();
@@ -21,7 +33,7 @@ fn p1() {
         })
         .collect::<HashMap<_, _>>();
 
-    let mut gates = gates_string
+    let gates = gates_string
         .lines()
         .map(|line| {
             let (inp, out) = line.split_once(" -> ").unwrap();
@@ -40,7 +52,11 @@ fn p1() {
             (out.to_string(), gate)
         })
         .collect::<HashMap<_, _>>();
+    (results, gates)
+}
 
+fn p1() {
+    let (mut results, mut gates) = load_input();
     while !gates.is_empty() {
         let new_results = gates
             .iter()
@@ -73,6 +89,104 @@ fn p1() {
     println!("P1: {p1}");
 }
 
+fn gen_graphviz(inputs: &HashMap<String, u8>, gates: &HashMap<String, Gate>) {
+    println!("digraph {{");
+    gates.iter().for_each(|(out, g_out)| {
+        gates
+            .iter()
+            .filter(|(_, g)| g.in1 == *out || g.in2 == *out)
+            .for_each(|(_, g)| {
+                println!(
+                    "{} -> {} [label=\"{}\"]",
+                    g_out.gate_name(),
+                    g.gate_name(),
+                    out
+                );
+            });
+        if !gates.iter().any(|(_, g)| g.in1 == *out || g.in2 == *out) {
+            println!("{} -> {}", g_out.gate_name(), out);
+        }
+        let lbl = match g_out.op {
+            '|' => "OR",
+            '&' => "AND",
+            '^' => "XOR",
+            _ => panic!("WTF"),
+        };
+        let shape = match g_out.op {
+            '|' => "square",
+            '&' => "triangle",
+            '^' => "diamond",
+            _ => panic!("WTF"),
+        };
+
+        println!("{} [shape={},label=\"{}\"]", g_out.gate_name(), shape, lbl);
+    });
+    inputs.iter().for_each(|(inp, _)| {
+        gates
+            .iter()
+            .filter(|(_, g)| g.in1 == *inp || g.in2 == *inp)
+            .for_each(|(_, g)| {
+                println!("{} -> {}", inp, g.gate_name());
+            });
+    });
+    println!("}}");
+}
+
+fn get_invalid_outputs(gates: &HashMap<String, Gate>) -> Vec<String> {
+    gates
+        .iter()
+        .filter(|&(out, gate)| {
+            if gate.op == '^' {
+                // start with x,y or output to z
+                if !((gate.in1.starts_with('x') && gate.in2.starts_with('y'))
+                    || (gate.in1.starts_with('y') && gate.in2.starts_with('x'))
+                    || out.starts_with('z'))
+                {
+                    return true;
+                }
+                // XOR that takes in x,y outputs to AND and XOR
+                if ((gate.in1.starts_with('x') && gate.in2.starts_with('y'))
+                    || (gate.in1.starts_with('y') && gate.in2.starts_with('x')))
+                    && gates
+                        .iter()
+                        .filter(|(_, g)| g.in1 == *out || g.in2 == *out)
+                        .any(|(_, g)| g.op == '|')
+                {
+                    return true;
+                }
+            }
+            // gate leading to output except last one is always XOR
+            if out.starts_with("z") && out != "z45" && gate.op != '^' {
+                return true;
+            }
+            // AND gate that takes input x,y except first one outputs to OR
+            if (gate.op == '&'
+                && (gate.in1 != "x00" && gate.in2 != "x00")
+                && ((gate.in1.starts_with('x') && gate.in2.starts_with('y'))
+                    || (gate.in1.starts_with('y') && gate.in2.starts_with('x'))))
+                && !gates
+                    .iter()
+                    .filter(|(_, g)| g.in1 == *out || g.in2 == *out)
+                    .all(|(_, g)| g.op == '|')
+            {
+                return true;
+            }
+
+            false
+        })
+        .map(|(out, _)| out.clone())
+        .collect::<Vec<_>>()
+}
+
+fn p2() {
+    let (_, gates) = load_input();
+    //gen_graphviz(&inputs, &gates);
+    let mut invalid = get_invalid_outputs(&gates);
+    invalid.sort();
+    println!("P2: {}", invalid.join(","));
+}
+
 fn main() {
     p1();
+    p2();
 }
